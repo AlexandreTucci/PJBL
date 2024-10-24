db14980c32da079d8207d83eb6083a4c70c6016d
João Victor Carvalho <joaovictor.crvl@gmail.com>
2024-10-23 21:48:15 -0300

    Update Jogador.java

--- Jogador.java
+++ Jogador.java
@@ -1,28 +1,24 @@
-public class Jogador {
+public class Jogador{
     private String nome;
-    private Fazenda fazenda;
+    private ArrayList<Fazenda> fazendas = new ArrayList<>();
 
     public Jogador(String nome) {
         this.nome = nome;
-        this.fazenda = new Fazenda("Minha Fazenda");
+        this.fazendas.add(new Fazenda()); 
     }
 
-    public void plantar(String nomePlanta) {
-        Planta planta = new Planta(nomePlanta, 0);
-        fazenda.adicionarSerVivo(planta);
-        System.out.println(nome + " plantou " + nomePlanta + " na fazenda.");
+    public void adicionarFazenda(Fazenda fazenda) {
+        this.fazendas.add(fazenda);
     }
 
-    public void alimentarAnimal(String nomeAnimal) {
-        for (SerVivo ser : fazenda.getSeresVivos()) {
-            if (ser instanceof Animal && ser.getNome().equals(nomeAnimal)) {
-                ((Animal) ser).setSaude(ser.getSaude() + 10);
-                System.out.println(nome + " alimentou " + nomeAnimal + ".");
-            }
-        }
+    public void comprarComida(){}
+    public void comprarAnimal(){}
+
+    public String getNome() {
+        return nome;
     }
 
-    public void mostrarStatusFazenda() {
-        fazenda.mostrarStatus();
+    public ArrayList<Fazenda> getFazendas() {
+        return fazendas;
     }
 }
